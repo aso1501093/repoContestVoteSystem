354529d139c105482cec6bdd215b65d121fa25c7
Ren <Ren@172.20.87.79>
2017-07-10 14:05:19 +0900

    コミット テスト

---
+++ src/vdao/CommentDAO.java
@@ -0,0 +1,63 @@
+package vdao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class CommentDAO {
+
+	DataSource ds = null;
+	Connection con = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+
+	public Connection connection() throws Exception {
+		if(ds == null){
+			ds = (DataSource)(new InitialContext()).lookup("java:comp/env/jdbc/MySQL");
+		}
+		con = ds.getConnection();
+
+		return con;
+	}
+
+	public void close() throws Exception{
+		if(rs != null){
+			rs.close();
+		}
+		if(stmt != null){
+			stmt.close();
+		}
+		if(con != null){
+			con.close();
+		}
+	}
+
+	//コメント投稿
+	public void insertComment(int art_id, int user_id, String comment){
+
+		try{
+			connection();
+
+			String sql = "INSERT INTO comment(art_id, user_id, comment)"
+					+ "VALUES (?,?,?)";
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, art_id);
+			stmt.setInt(2, user_id);
+			stmt.setString(3, comment);
+			int rs = stmt.executeUpdate();
+
+		}catch(Exception e){
+			System.out.println(e);
+		}finally{
+			try{
+				close();
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+	}
+
+}
