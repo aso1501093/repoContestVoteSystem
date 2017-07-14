49f954368f315b28848f1d7964f271152898a26c
1501124 <1501124@PC63324.asost.local>
2017-07-14 11:50:50 +0900

    コミットテスト

---
+++ src/cservlet/V2Addcomment.java
@@ -0,0 +1,80 @@
+package cservlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import cmodel.Art;
+import cmodel.Comment;
+import cmodel.User;
+
+
+/**
+ * Servlet implementation class V2Addcomment
+ */
+@WebServlet("/V2Addcomment")
+public class V2Addcomment extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	DataSource ds = null;
+	Connection con = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+    /**
+     * Default constructor.
+     */
+    public V2Addcomment() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-detail.jsp");
+		rd.forward(request, response);
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+
+
+		HttpSession session = request.getSession();
+//		CommentDAO commentDAO = new CommentDAO();
+		Comment c = new Comment();
+
+		//ユーザ情報と表示中作品情報取得
+		User user = (User)session.getAttribute("");
+		Art art = (Art) session.getAttribute("");
+
+		//コメント取得+modelへのセット
+		String comment = request.getParameter("");
+		c.setComment(comment);
+		c.setArt_id(art.getArt_id());
+		c.setUser_id(user.getUser_id());
+		//コメントの登録
+		commentDAO.addComment(c);
+
+		//作品に投稿されているコメントを取得
+//		c=commentDAO.;
+		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-detail.jsp");
+		rd.forward(request, response);
+	}
+
+
