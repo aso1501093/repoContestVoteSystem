96160ec0d9133b768556bde6181bf66d0ef706e6
1501124 <1501124@PC63324.asost.local>
2017-07-24 11:50:08 +0900

    Addcomment

--- src/cservlet/V2Addcomment.java
+++ src/cservlet/V2Addcomment.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.naming.InitialContext;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,10 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-import cdao.CommentDAO;
-import cmodel.Art;
 import cmodel.Comment;
-import cmodel.User;
 
 
 /**
@@ -56,26 +54,79 @@ public class V2Addcomment extends HttpServlet {
 
 
 		HttpSession session = request.getSession();
-		CommentDAO commentDAO = new CommentDAO();
+//		CommentDAO commentDAO = new CommentDAO();
 		Comment c = new Comment();
 
 		//ユーザ情報と表示中作品情報取得
-		User user = (User)session.getAttribute("user");
-		Art art = (Art) session.getAttribute("artdata");
+//		User user = (User)session.getAttribute("user");
+//		Art art = (Art) session.getAttribute("artdata");
 
 		//コメント取得+modelへのセット
-		String comment = request.getParameter("");
+		String comment = request.getParameter("comment");
 		c.setComment(comment);
-		c.setArt_id(art.getArt_id());
-		c.setUser_id(user.getUser_id());
+//		c.setArt_id(art.getArt_id());
+//		c.setUser_id(user.getUser_id());
 		//コメントの投稿
-		commentDAO.insertComment(c,comment);
+		//commentDAO.insertComment(c,comment);
 
-		//作品に投稿されているコメントを取得
-//		c=commentDAO.;
+		c.setArt_id(1);
+		c.setUser_id(1501155);
+		insertComment(c,comment);
+
+
+
 		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-detail.jsp");
 		rd.forward(request, response);
 	}
+
+
+	/////////////////////////////insertDAOの処理///////////////////////////////////////////////
+	public Connection connection() throws Exception {
+		if(ds == null){
+			ds = (DataSource)(new InitialContext()).lookup("java:comp/env/jdbc/MySQL");
+		}
+		con = ds.getConnection();
+
+		return con;
+	}
+	public void close() throws Exception{
+		if(rs != null){
+			rs.close();
+		}
+		if(stmt != null){
+			stmt.close();
+		}
+		if(con != null){
+			con.close();
+		}
+	}
+	public void insertComment(Comment c, String comment){
+
+		int art_id = c.getArt_id();
+		int user_id = c.getUser_id();
+
+		try{
+			connection();
+
+			String sql = "INSERT INTO comment(art_id, user_id, comment)"
+					+ "VALUES (?,?,?)";
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, art_id);
+			stmt.setInt(2, user_id);
+			stmt.setString(3, comment);
+			stmt.executeUpdate();
+
+		}catch(Exception e){
+			System.out.println(e);
+		}finally{
+			try{
+				close();
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+	}
+	//////////////////////////////////////////////////////////////////////////////////
 }
 
 
