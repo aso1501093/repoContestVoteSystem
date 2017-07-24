dab75f1f017bc7f88880bf41783b3273b5afc74c
ishibashieisaku <1501093@st.asojuku.ac.jp>
2017-07-24 11:46:12 +0900

    あ

--- src/cdao/ContestDAO.java
+++ src/cdao/ContestDAO.java
@@ -3,11 +3,16 @@ package cdao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 import cmodel.Comment;
+import cmodel.Contest;
+import cmodel.User;
 
 public class ContestDAO {
 	DataSource ds = null;
@@ -33,6 +38,85 @@ public class ContestDAO {
 		if(con != null){
 			con.close();
 		}
+	}
+
+
+	public ArrayList<Contest> getVoteContest(String date){
+		
+		ArrayList<Contest> list = new ArrayList<>();
+		Contest contest;
+
+		try{
+			connection();
+
+			String sql = "SELECT * FROM contest "
+					+ "WHERE vote_start_date <= ? AND "
+					+ "vote_end_date >= ?";
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, date);
+			stmt.setString(2, date);
+			rs = stmt.executeQuery();
+			
+			
+			while(rs.next()){
+				contest = new Contest();
+				contest.setContest_id(rs.getInt("contest_id"));
+				contest.setName(rs.getString("contest_name"));
+				contest.setStart_date(rs.getDate("vote_start_date"));
+				contest.setEnd_date(rs.getDate("vote_end_date"));
+				list.add(contest);
+			}
+
+		}catch(Exception e){
+			contest = null;
+			System.out.println("databaseerror"+e);
+		}finally{
+			try{
+				close();
+			}catch(Exception e){
+				System.out.println("2databaseerror"+e);
+			}
+		}
+
+		return list;
+	}
+	
+	public ArrayList<Contest> getPastContest(String date){
+		
+		ArrayList<Contest> list = new ArrayList<>();
+		Contest contest;
+
+		try{
+			connection();
+
+			String sql = "SELECT * FROM contest "
+					+ "WHERE vote_end_date < ?";
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, date);
+			rs = stmt.executeQuery();
+			
+			
+			while(rs.next()){
+				contest = new Contest();
+				contest.setContest_id(rs.getInt("contest_id"));
+				contest.setName(rs.getString("contest_name"));
+				contest.setStart_date(rs.getDate("vote_start_date"));
+				contest.setEnd_date(rs.getDate("vote_end_date"));
+				list.add(contest);
+			}
+
+		}catch(Exception e){
+			contest = null;
+			System.out.println("databaseerror"+e);
+		}finally{
+			try{
+				close();
+			}catch(Exception e){
+				System.out.println("2databaseerror"+e);
+			}
+		}
+
+		return list;
 	}
 
 
