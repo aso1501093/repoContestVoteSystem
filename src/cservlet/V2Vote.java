52af0309ffd9fb8f2ec3720e3576ac638d37bce3
1501124 <1501124@PC63324.asost.local>
2017-07-25 15:15:40 +0900

    データベース確認済

--- src/cservlet/V2Vote.java
+++ src/cservlet/V2Vote.java
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import cdao.VoteDAO;
-import cmodel.Art;
-import cmodel.Contest;
-import cmodel.User;
 import cmodel.Vote;
 
 /**
@@ -37,6 +34,29 @@ public class V2Vote extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+
+//		User user = (User) session.getAttribute("user");
+//		int contestId = (int) session.getAttribute("contestId");
+//		int artId = (int) session.getAttribute("artId");
+
+		Vote v = new Vote();
+
+
+		int userId = 1501155;
+		int artId = 1;
+		int contestId =1;
+		//modelにセット
+		v.setArt_id(artId);
+		v.setContest_id(contestId);
+//		v.setUser_id(user.getUser_id());
+		v.setUser_id(userId);
+		//投票実行
+		VoteDAO voteDAO = new VoteDAO();
+		voteDAO.Vote(v);
+
+
+
 		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-artdetail.jsp");
 		rd.forward(request, response);
 	}
@@ -49,17 +69,21 @@ public class V2Vote extends HttpServlet {
 		//doGet(request, response);
 		HttpSession session = request.getSession();
 
-		User user = (User) session.getAttribute("user");
-		Contest contest = (Contest) session.getAttribute("");
-		Art art = (Art) session.getAttribute("artdata");
+//		User user = (User) session.getAttribute("user");
+//		int contestId = (int) session.getAttribute("contestId");
+//		int artId = (int) session.getAttribute("artId");
 
 		Vote v = new Vote();
 
 
+		int userId = 1501155;
+		int artId = 1;
+		int contestId =1;
 		//modelにセット
-		v.setArt_id(art.getArt_id());
-		v.setContest_id(contest.getContest_id());
-		v.setUser_id(user.getUser_id());
+		v.setArt_id(artId);
+		v.setContest_id(contestId);
+//		v.setUser_id(user.getUser_id());
+		v.setUser_id(userId);
 		//投票実行
 		VoteDAO voteDAO = new VoteDAO();
 		voteDAO.Vote(v);
