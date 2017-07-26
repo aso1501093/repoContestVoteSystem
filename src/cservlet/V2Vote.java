5edf1975ac796fe222d8efabe19df8d1cc69eac5
1501124 <1501124@PC63324.asost.local>
2017-07-26 12:45:48 +0900

    投票テスト

--- src/cservlet/V2Vote.java
+++ src/cservlet/V2Vote.java
@@ -16,7 +16,7 @@ import cmodel.Vote;
 /**
  * Servlet implementation class Vote
  */
-@WebServlet("/Vote")
+@WebServlet("/V2Vote")
 public class V2Vote extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -34,31 +34,34 @@ public class V2Vote extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		HttpSession session = request.getSession();
-
+//		HttpSession session = request.getSession();
+//
 //		User user = (User) session.getAttribute("user");
 //		int contestId = (int) session.getAttribute("contestId");
 //		int artId = (int) session.getAttribute("artId");
-
-		Vote v = new Vote();
-
-
-		int userId = 1501155;
-		int artId = 1;
-		int contestId =1;
-		//modelにセット
-		v.setArt_id(artId);
-		v.setContest_id(contestId);
+//		System.out.println("セッションから取得したアートID"+artId);
+//
+//		Vote v = new Vote();
+//
+//
+//		int userId = 1501155;
+//
+//
+//
+//		//modelにセット
+//		v.setArt_id(artId);
+//		System.out.println("モデルにセットしたアートID"+v.getArt_id());
+//		v.setContest_id(contestId);
 //		v.setUser_id(user.getUser_id());
-		v.setUser_id(userId);
-		//投票実行
-		VoteDAO voteDAO = new VoteDAO();
-		voteDAO.Vote(v);
-
-
-
-		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-artdetail.jsp");
-		rd.forward(request, response);
+//		v.setUser_id(userId);
+//		//投票実行
+//		VoteDAO voteDAO = new VoteDAO();
+//		voteDAO.Vote(v);
+//
+//
+//
+//		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-artdetail.jsp");
+//		rd.forward(request, response);
 	}
 
 	/**
@@ -70,17 +73,20 @@ public class V2Vote extends HttpServlet {
 		HttpSession session = request.getSession();
 
 //		User user = (User) session.getAttribute("user");
-//		int contestId = (int) session.getAttribute("contestId");
-//		int artId = (int) session.getAttribute("artId");
+		int contestId = (int) session.getAttribute("contestId");
+		int artId = (int) session.getAttribute("artId");
+		System.out.println("セッションから取得したアートID"+artId);
 
 		Vote v = new Vote();
 
 
 		int userId = 1501155;
-		int artId = 1;
-		int contestId =1;
+
+
+
 		//modelにセット
 		v.setArt_id(artId);
+		System.out.println("モデルにセットしたアートID"+v.getArt_id());
 		v.setContest_id(contestId);
 //		v.setUser_id(user.getUser_id());
 		v.setUser_id(userId);
@@ -92,7 +98,6 @@ public class V2Vote extends HttpServlet {
 
 		RequestDispatcher rd =request.getRequestDispatcher("WEB-INF/jsp/V2-artdetail.jsp");
 		rd.forward(request, response);
-
 	}
 
 }
